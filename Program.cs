97ab4f99be00fdc150ba2282beb64134d0db857c
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-25 16:43:50 +0300

    Решение задачи №3 ДЗ Семинар №3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Enter a value: ");
+long Value = Convert.ToInt64(Console.ReadLine());
+for (int i = 1; i <= Value; i++)
+{
+    double CubicValue = Math.Pow(i, 3);
+    Console.Write($"{CubicValue}, ");
+}
